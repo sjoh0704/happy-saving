b804dc68041160411a8b4906301daa731e5b3e4e
sjoh0704 <sjoh0704@daum.net>
2022-10-16 02:20:59 +0900

    [feat] add connect couple function

---
+++ front/src/components/ConnectCouple.js
@@ -0,0 +1,58 @@
+const ConnectCouple = (props) =>{
+    const userId = localStorage.getItem("userid")
+    const coupleInfo = props.coupleInfo
+
+    console.log(111, coupleInfo)
+    if (coupleInfo == null){
+        return (
+        <div>
+            당신은 아직 couple 등록이 안되어 있습니다!
+            couple 신청을 해야합니다. 
+            <p>
+                <a href="/couple/make">
+                couple 신청     
+                </a>
+            </p>
+
+        </div>)
+    }else if (coupleInfo.send_id == userId){
+        if (coupleInfo.phase == "awaiting"){
+            return (
+                <div>
+                   <p>
+                        {coupleInfo.receiver.name} 님의 수락을 기다리고 있습니다.
+                        기다려 주세요.
+                    </p>
+                </div>
+            )
+        }else if (coupleInfo.phase == "denyed"){
+            return (
+                <div>
+                    거절됨
+                </div>
+            )
+        }else{
+            return (
+                <div>
+                    승인됨
+                </div>
+            )
+        }
+
+    }else if (coupleInfo.recv_id == userId){
+        return (
+            <div>
+            
+            </div>
+        )
+    }else{
+        return (
+            <div>
+                error
+            </div>
+        )
+    }
+
+}
+
+export default ConnectCouple
--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,5 +1,6 @@
 import {React, useEffect, useState} from "react";
 import axios from 'axios';
+import ConnectCouple from "../components/ConnectCouple";
 
 const Home =() => {
 
@@ -15,11 +16,8 @@ const Home =() => {
         }
         try {
             let res = await axios.get("/apis/v1/couples?userid=" + userId)
-            console.log(res)
             let payload = res.data.payload
-            console.log(payload)
-            
-            setCoupleInfo(payload.phase)
+            setCoupleInfo(payload)
 
         } catch (err) {
             console.log(err)
@@ -37,22 +35,8 @@ const Home =() => {
         <h3>
             안녕하세요! {name} 님!
         </h3>
-
         <div>
-            {coupleInfo == null ? 
-            <div>
-            당신은 아직 couple 등록이 안되어 있습니다!
-            couple 신청을 해야합니다. 
-            <p>
-                <a href="/couple/make">
-                couple 신청     
-                </a>
-            </p>
-            </div>:
-            <div>
-                {coupleInfo}
-                
-            </div>}
+            <ConnectCouple coupleInfo={coupleInfo}/>
         </div>
     </div>
     );
